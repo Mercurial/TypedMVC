abe4fd611f13458b6195dcaa051bd88f1ba6d460
Clark Alesna <clark_alesna@hotmail.com>
2018-03-07 21:21:38 +0800

    Added Base Model Class

--- Framework/TypedMVC.ts
+++ Framework/TypedMVC.ts
@@ -2,3 +2,4 @@ export * from './Controllers/BaseController';
 export * from './Controllers/ControllerManager';
 export * from './Interfaces/IController';
 export * from './Log/Logger';
+export * from './Models/BaseModel';
